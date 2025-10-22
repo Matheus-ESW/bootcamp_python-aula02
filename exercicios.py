f50d2773ae4d614846ef4a26c16967a83f3d9921
Matheus Ramos <matheussoaresramos@gmail.com>
2025-10-21 21:04:45 -0300

    Finalizados os execicios sobre inteiros

---
+++ exercicios.py
@@ -0,0 +1,16 @@
+# inteiros
+
+# ex1
+# print("Soma dos números: " + str( int(input("Digite um número inteiro: ")) + int(input("Digite outro número inteiro: "))) )
+
+# ex2
+# print("Resto da divisão: " + str(int(input("Digite um número inteiro: ")) % 5 ))
+
+# ex3
+# print("Resultado da multiplicação: " + str( (int(input("Digite um número inteiro: "))) * (int(input("Digite outro número inteiro: "))) ))
+
+# ex4
+# print("Divisão inteira: " + str( (int(input("Digite um número inteiro: "))) // (int(input("Digite outro número inteiro: "))) ))
+
+# ex5
+print("Quadrado do número: " + str( (int(input("Digite um número inteiro: "))) ** 2 ))
