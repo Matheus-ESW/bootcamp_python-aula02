d5636c002d81b29836e7b49b826e87d46490a481
Matheus Ramos <matheussoaresramos@gmail.com>
2025-10-21 21:07:22 -0300

    Finalizados os exercicios sobre flutuantes

--- exercicios.py
+++ exercicios.py
@@ -13,4 +13,21 @@
 # print("Divisão inteira: " + str( (int(input("Digite um número inteiro: "))) // (int(input("Digite outro número inteiro: "))) ))
 
 # ex5
-print("Quadrado do número: " + str( (int(input("Digite um número inteiro: "))) ** 2 ))
+# print("Quadrado do número: " + str( (int(input("Digite um número inteiro: "))) ** 2 ))
+
+# flutuantes
+
+# ex1
+# print("Soma dos números: " + str( float(input("Digite um número flutuante: ")) + float(input("Digite outro número flutuante: ")) ) )
+
+# ex2
+# print("Média dos números: " + str( ( float(input("Digite um número flutuante: ")) + float(input("Digite outro número flutuante: ")) ) / 2 ) )
+
+# ex3
+# print("Resultado da potência: " + str( (float(input("Digite um número flutuante base: "))) ** (float(input("Digite outro número flutuante expoente: "))) ))
+
+# ex4
+# print("Conversão de Celsius para Fahrenheit: " + str( (float(input("Digite a temperatura em Celsius: ")) * 9/5) + 32 ) )
+
+# ex5
+print("Area de um círculo: " + str( 3.14 * (float(input("Digite o raio do círculo: ")) ** 2) ) )
