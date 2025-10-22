cf3a806cf9f96103321671b52ce80c6834299403
Matheus Ramos <matheussoaresramos@gmail.com>
2025-10-21 21:35:07 -0300

    Finalizados os exercicios sobre strings

--- exercicios.py
+++ exercicios.py
@@ -30,4 +30,21 @@
 # print("Conversão de Celsius para Fahrenheit: " + str( (float(input("Digite a temperatura em Celsius: ")) * 9/5) + 32 ) )
 
 # ex5
-print("Area de um círculo: " + str( 3.14 * (float(input("Digite o raio do círculo: ")) ** 2) ) )
+# print("Area de um círculo: " + str( 3.14 * (float(input("Digite o raio do círculo: ")) ** 2) ) )
+
+# strings
+
+# ex1
+# print("Convertendo para maiúsculas: " + str( input("Digite uma string: ").upper() ) )
+
+# ex2
+# print("Nome com as letras minúsculas: " + str( input("Digite seu nome: ").lower() ) )  
+
+# ex3
+# print("Resultado com espaços removidos nas extremidades: " + str( input("Digite uma frase: ").strip() ) )
+
+# ex4
+# print("imprimindo dia, mês e ano separadamente: " + str( input("Digite uma data no formato DD/MM/AAAA: ").split("/") ) )
+
+# ex5
+print("Concatenando duas strings: " + str( input("Digite a primeira string: ") + " " + input("Digite a segunda string: ") ) )
